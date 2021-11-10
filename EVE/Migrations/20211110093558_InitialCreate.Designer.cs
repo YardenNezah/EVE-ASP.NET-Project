936a8b9b2d28831c45cfd74fa7e9bd0fa3c4f517
aviv275 <34794052+aviv275@users.noreply.github.com>
2021-11-10 17:05:56 +0200

    maps + twitter

--- EVE/Migrations/20211110093558_InitialCreate.Designer.cs
+++ EVE/Migrations/20211110093558_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using EVE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EVE.Migrations
 {
     [DbContext(typeof(EVEContext))]
-    partial class EVEContextModelSnapshot : ModelSnapshot
+    [Migration("20211110093558_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EVE/Migrations/20211110093558_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EVE.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    countrey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    houseNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
