4e242275359ff311f1f00a496516e81e1f1b3a97
aviv275 <34794052+aviv275@users.noreply.github.com>
2021-11-13 21:22:34 +0200

    products last changes

--- EVE/wwwroot/js/ProductIndex.js
+++ EVE/wwwroot/js/ProductIndex.js
@@ -1,16 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-//Made By: David Manshari! at 4am :(
-
-// Write your JavaScript code.
-
-
+﻿
 $(function () {
 
-    var skipCount = 0;
-    var takeCount = 6;
     var hasMoreRecords = true;
     var mode = 0;
-    var brand = 0;
     var category = 0;
     var sort = "newest";
     showProducts();
@@ -24,7 +16,7 @@ $(function () {
             success: function (data) {
                 if (data == 0) {
                     $('#productInsertion').html("");
-                    hasMoreRecords = false; // signal no more records to display
+                    hasMoreRecords = false; 
                     $('#ShowMoreProducts').text("No More Products To Show").css('background-color', 'red');
                 }
                 else {
